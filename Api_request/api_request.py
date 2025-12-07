a36f11b7fc34e9221f38fd73e5775b7e7cc85dfd
Meena M <128904541+Meena123M@users.noreply.github.com>
2025-12-07 15:52:45 +0530

    Add files via upload

---
+++ Api_request/api_request.py
@@ -0,0 +1,28 @@
+# import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+
+
+
+api_key='your api key'
+
+ticker='AAPL'
+
+ts=TimeSeries(key=api_key,)
+
+#get daily stocks
+data,meta_data=ts.get_daily(symbol=ticker,outputsize='compact')
+
+print(data)
+
+# fetch data using function
+def fetch_daily_stock_data(ticker_symbol):
+    ts = TimeSeries(key=api_key,)
+    data, meta_data = ts.get_daily(symbol=ticker_symbol, outputsize='compact')
+    return data
+
+
+
+
+
+
+
